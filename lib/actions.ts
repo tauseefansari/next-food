b9e8dec45b7405cfffb7dfa26c13fe220befa8a6
Tauseef Ansari <tauseeftanvir@gmail.com>
2024-07-20 21:17:37 +0530

    fix(next-food): :bug: Revalidate Next Cache to show latest meals

--- lib/actions.ts
+++ lib/actions.ts
@@ -5,6 +5,7 @@ import { MealsFormState, MealStoreInputs } from "@/model/meal";
 import { saveMeal } from "./meals";
 import { redirect } from "next/navigation";
 import appRoutes from "@/common/appRoutes";
+import { revalidatePath } from "next/cache";
 
 const isInvalidText = (textOrFile: string | File) => {
   if (textOrFile instanceof File) return !textOrFile || textOrFile.size === 0;
@@ -28,5 +29,6 @@ export const shareMeal = async (
     return { message: "Invalid Input, please fill all the input values" };
 
   await saveMeal(meal);
+  revalidatePath(appRoutes.MEALS);
   redirect(appRoutes.MEALS);
 };
